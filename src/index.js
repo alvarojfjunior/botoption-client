2e2f4a55a4a5aa55bb22c184d583355b375b7921
Alvaro <alvaro@sg.srv>
2021-07-13 14:44:09 -0700

    fix socket

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const { connect, runSignal } = require("./iqoption");
 const { handleAuth, } = require("./credentials");
-socket = require("socket.io-client"), io = socket.connect("http://localhost:8000");
+const socket = require("socket.io-client")
+const io = socket('https://botoption-server.herokuapp.com')
+
+
 let isRunningSignal = false;
 try {
     handleAuth().then(async credentials => {
         if (!credentials.auth) return;
-        const page = await connect(credentials.iqEmail, credentials.iqPassword);
-        if (!page) throw ""
+        // const page = await connect(credentials.iqEmail, credentials.iqPassword);
+        // if (!page) throw ""
         io.on("signal", async msg => {
             if (!isRunningSignal) {
                 isRunningSignal = true;
